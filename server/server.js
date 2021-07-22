77b581de93b8ff0fb992bad7149451070f05b8ac
joellekado <jkado@my.hpu.edu>
2021-07-22 16:56:56 -0500

    set up routes for CRUD

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const storyRouter = require('./routes/story.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/story', storyRouter); //this route will be used when we Get art
 
 // Serve static files
 app.use(express.static('build'));
