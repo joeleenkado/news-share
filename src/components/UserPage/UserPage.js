7fd07748a3810d05417cbdca7c3baa43bc393a1a
joellekado <jkado@my.hpu.edu>
2021-07-22 19:02:28 -0500

    home page 'GET's stories

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -5,9 +5,15 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class UserPage extends Component {
   // this component doesn't do much to start, just renders some user info to the DOM
+  componentDidMount() {
+    this.props.dispatch({ type: 'FETCH_STORY' });
+  }
+
   render() {
     return (
       <div>
+        REDUX STATE: {JSON.stringify(this.props.store)}
+
         <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
         <p>Your ID is: {this.props.store.user.id}</p>
         <LogOutButton className="log-in" />
